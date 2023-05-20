70e814f6f80618c047a84a3ce5be2c913135b2f8
arpple <apple0239@gmail.com>
2023-05-20 11:15:48 +0900

    fix cron config

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,10 @@ import Config
 
 config :goons_tracker_bot, GoonsTrackerBot.Scheduler,
 	jobs: [
-		{"*/30 * * * *", fn -> GoonsTrackerBot.update_location() end}
+		update_location: [
+			schedule: "*/30 * * * *",
+			task: {GoonsTrackerBot, :update_location, []},
+		],
 	]
 
 import_config "#{config_env()}.secret.exs"
